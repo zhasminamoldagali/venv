a1a9e52f29d978d3df47d849c59e827cc0c15209
la <you@example.com>
2026-03-10 12:17:06 +0500

    lalal

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,27 @@
+from django.contrib import admin
 from django.db import models
 import datetime
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-    def __str__(self):
-        return self.question_text
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+        )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
